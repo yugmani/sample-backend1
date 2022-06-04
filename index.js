f9596bcb01aa1edc8b480098d563e063883cdc75
Himalaya <yugmani@yahoo.com>
2022-06-04 14:00:19 -0700

    figlet added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //importing node's built-in web server module
 const http = require("http");
+const figlet = require("figlet");
 
 let notes = [
   {
@@ -25,9 +26,35 @@ let notes = [
 //creating a server using createServer method of the http module
 const app = http.createServer((request, response) => {
   //The request responded with the status code with the content-type header set to text/plain(application/json)
-  response.writeHead(200, { "Content-Type": "application/json" });
-  //the content of the site to be returned set to 'Hellow World!'(notes)
-  response.end(JSON.stringify(notes));
+
+  //   response.writeHead(300);
+  let url = request.url;
+
+  if (url === "/" || url === "/home") {
+    //the content of the site to be returned set to 'Hellow World!'(notes)
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.write(JSON.stringify(notes));
+  } else if (url === "/about") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(
+      "<div><h1>About Us</h1><h3>We have the best Tech Support</h3></div>"
+    );
+  } else if (url === "/contact") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("<div><h1>Contact</h1><h3>ph:3333333333</h3></div>");
+  } else {
+    figlet("404!!!", function (err, data) {
+      if (err) {
+        console.log("Something wrong on the server");
+        console.dir(err);
+        return;
+      }
+      response.write(data);
+      response.end();
+    });
+  }
+
+  response.end();
 });
 
 //port number is set to 3001
